e03426b13c3706ea4a869a186049b6728ba6a848
Nicholas Heo <nicholas.heo95@gmail.com>
2020-06-25 14:29:20 -0400

    progress 06252020

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const port = 3333;
 const nameHandler = require("./controller/controllerIndex.js");
 
 app.get("/getName", nameHandler.getName2);
+app.get("/getIP", (req, res) => {
+  res.send(req.ip);
+});
 
 app.listen(port, function (err) {
   if (err) {
